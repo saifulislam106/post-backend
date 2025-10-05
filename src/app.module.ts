98e99225ddab227a07531c03c30cfc584562eab1
Saiful Islam <saifulislam106915@gmail.com>
2025-10-05 23:59:37 +0600

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,7 @@ import { UserModule } from './main/user/user.module';
 import { LikeModule } from './main/like/like.module';
 import { MessageModule } from './main/message/message.module';
 import { FirebaseModule } from './main/firebase/firebase.module';
-import {ConfigModule} from '@nestJs/config';
+import {ConfigModule} from '@nestjs/config';
 
 @Module({
   imports: [
